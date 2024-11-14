4871ecc2062c1e29f27d2634359d9a06cd5ca57e
ANAVITORIA2234 <ana.v.duarte6@aluno.senai.br>
2024-11-14 17:32:14 -0300

    v3
    atualizando matérias da aula

---
+++ Javascript/Javscript - aula1/scripts/somar.js
@@ -0,0 +1,14 @@
+var n1 = prompt('Digite o primeiro numero')
+var n2 = prompt('Digite o segundo  numero')
+
+//Number() - transformar texto em numero
+var soma = Number(n1) + Number(n2) 
+
+alert("A soma dos numeros é de:  "+soma)
+
+// let - global e sobrescrevivel
+var variavel_global = 1
+// let - local e sobrescrevivel
+let variavel_local   = 3
+// let - global e sobrescrevivel
+const variavel_constante ="fixo"
---
+++ Javascript/Javscript - aula1/scripts/teste.js
@@ -0,0 +1,18 @@
+// caixa de tempo
+// alert('Hello world!')
+// prompt('Qual e o seu nome')
+// confirm('Deseja baixar novamente')
+
+// tipos de variavel
+// string - caractere
+// number - numero (inteiro,real)
+// boolean - logico ( verdadeiro  falso)
+// array - vetor (lista)
+var nome= prompt('Qual é o seu nome')
+var numero_faz = 11
+var ligado = false
+var valor_total = 1.7
+var lista ={ }
+
+var aniversario = prompt('Qual é o dia do aniversario')
+alert('Olá' +nome+ 'o dia do seu aniversario é' +aniversario)
