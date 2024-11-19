40ecf6cd3cca7e1d4c850cae43ce7e98deffd337
ANAVITORIA2234 <ana.v.duarte6@aluno.senai.br>
2024-11-19 17:37:23 -0300

    v2
    atualizando matérias da aula

---
+++ Javascript/aula3- repedicaoedom/script.js
@@ -0,0 +1,27 @@
+// DOM
+// URL  - LOCATION
+//  "BOTAO DE VOLTAR" - HISTORY
+//  "SITE / CONTEUDO" - DOCUMENT
+
+// windon.location.reload()
+// window.location = "https://www.google.com"
+
+// document - documento
+// getElementByld - busca o sujeito pelo id
+// inner.. - coloca uma elemento dentro
+// innerHTML -  texto o codigo
+
+document.getElementById('catalogo').innerHTML = 'h3>01<h3></h3>'
+document.querySelector('catalogo').innerHTM   = 'ola'
+
+function registrarNome(){
+document.querySelector('sinputNome').value
+alert('O seu nome e' +nome)
+
+document.querySelector('catalogo').innerHTM   = `
+<div id="nomeDiv"></div>
+ eu $(Ana) sou muito top e programadora!
+ </div>
+
+`
+}
