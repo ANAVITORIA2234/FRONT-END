afbfead67c3ae2de2601f038ce5a8c2ff61136e2
ANAVITORIA2234 <ana.v.duarte6@aluno.senai.br>
2025-02-27 17:36:54 -0300

    aula 1
    atividade do php

---
+++ backEnd/livros/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LIVRARIA</title>
+    <link rel="stylesheet" href="styles/livraria.css">
+</head>
+
+<body>
+    <div id="cabecalho">
+        <h1>2025</h1>
+    </div>
+    <div id="conteudo">
+        <h2>LIVRARIA</h2>
+        <img src="images/livro.jpg" >
+        </p>nome do livro: Para todas as pessoas intensas. </p>
+        </p>nome da autora: Albuquerque Iandê  </p>
+</p> descrição:“Um dia alguém vai sumir da sua vida, só porque você é intenso demais, ou porque você é simplesmente amor demais. E algumas pessoas têm medo do amor. ser intenso é ser profundo demais, imenso demais, vivo demais, e hoje em dia, as pessoas têm um medo danado disso.</p>
+    </div>
+
+    <div id="rodape">
+        <p> Todos os direitos reservados a @Ana</p>
+    </div>
+
+</body>
+
+</html>
