363d1f8972e4a0f794ede11374e5dd17a0db1ae1
ANAVITORIA2234 <ana.v.duarte6@aluno.senai.br>
2025-03-25 17:52:33 -0300

    v1

---
+++ comida/db/config.php
@@ -0,0 +1,8 @@
+<?php
+ //  CONSTANTES QUE SALVAM A CONFIGURAÇÃO DE CONEXÃO COM O BANCO
+DEFINE("SERVIDOR" ,"localhost");
+DEFINE("USUARIO" ,"root");
+DEFINE("SENHA" ,"");
+DEFINE("BANCO" ,"comida");
+
+?>
