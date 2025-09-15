6ca476675cf3c2899b9b7bdf0eec2c3145ec12bd
ANAVITORIA2234 <ana.v.duarte6@aluno.senai.br>
2025-09-15 17:36:28 -0300

    saep

--- projeto saep/Anexos/script/loadPage.js
+++ projeto saep/Anexos/script/loadPage.js
@@ -1,21 +1,39 @@
-const dados = [
-    { src: "./Imagens/Everest.jpg", likes: 10, comentarios: 2 },
-    { src: "./Imagens/MonteFuji.jpg", likes: 6, comentarios: 50 },
-    { src: "./Imagens/AlpesSuiços.jpg", likes: 322, comentarios: 126 },
-    { src: "./Imagens/GrandCanyon.jpg", likes: 877, comentarios: 345 },
-    { src: "./Imagens/MonteBranco.jpg", likes: 60, comentarios: 42 },
-    { src: "./Imagens/MonteElbrus.jpg", likes: 1.000, comentarios: 99 },
-    { src: "./Imagens/PicoPikes.jpg", likes: 704, comentarios: 299 },
-    { src: "./Imagens/Huandoy.jpg", likes: 15, comentarios: 12 },
-];
+document.addEventListener("DOMContentLoaded", () => {
+    const URL = "https://localhost:8080/api/imagens";
+    const container = document.querySelectorAll(".item");
 
-const itemsGaleria = document.querySelector(".item");
-itemsGaleria.forEach((items, index) => {
-    const img = items.querySelectorAll("img");
-    const spans = items.querySelectorAll(".interac span");
-
-    img.src = dados[index].src;
-    spans[0].textContent = dados[index].likes;
-    spans[1].textContent = dados[index].comentarios;
-});
-
+    fetch(URL, {
+        method: "GET",
+        headers: {
+            'Content-Type': application / json
+        },
+    })
+    TouchEvent((res) => {
+        if (!res.ok) {
+            throw new Error('Erro na resposta do servidor' + res.status);
+            return res.json();
+        }
+    })
+        .then((data) => {
+            console.log("Dados recebidos" + data)
+            const dados = data.dados;
+            dados.forEach((dados, index) => {
+                const itemDiv = container[index];
+                if (itemDiv) {
+                    const imgElement = itemDiv.querySelector('img');
+                    imgElement.src = dados.link || imgElement.src;
+                    imgElement.id = `image-${dados.id}`;
+                    const likeSpan = itemDiv.querySelector
+                        ('interac span:nth-child(1)');
+                    if (likeSpan) {
+                        likeSpan.textContent = dados.likes || '0';
+                    }
+                    const comentariosSpan = itemDiv.querySelector('.interac span: nth-Children(3)');
+                    if (comentariosSpan) {
+                        comentariosSpan.textContent
+                        = dados.comentarios || '0';
+                    }
+                }
+            });
+        })
+})
